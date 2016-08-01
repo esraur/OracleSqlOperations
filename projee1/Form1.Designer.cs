6a6b39c704e7b1bbb0f9805ada3a23f5293147f6
Esra Ur <esraur@yandex.com>
2016-08-01 21:39:51 +0300

    First commit

---
+++ projee1/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace projee1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(133, 115);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Kaydet";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton1.ForeColor = System.Drawing.Color.Maroon;
+            this.radioButton1.Location = new System.Drawing.Point(465, 45);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(66, 17);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "ENBAS";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton2.ForeColor = System.Drawing.Color.Maroon;
+            this.radioButton2.Location = new System.Drawing.Point(606, 45);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(68, 17);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "ENSON";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(505, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Kimden:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.textBox3.Location = new System.Drawing.Point(574, 87);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(529, 115);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Kaydet";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(246, 37);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(129, 25);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "BİRLEŞTİR";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.AcceptsReturn = true;
+            this.textBox2.AcceptsTab = true;
+            this.textBox2.AllowDrop = true;
+            this.textBox2.Location = new System.Drawing.Point(412, 144);
+            this.textBox2.MaxLength = 20000000;
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(321, 252);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.AcceptsTab = true;
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Location = new System.Drawing.Point(41, 144);
+            this.textBox1.MaxLength = 20000000;
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(321, 252);
+            this.textBox1.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(782, 459);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ projee1/Form1.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Globalization;
+using System.Text.RegularExpressions;
+namespace projee1
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //TOADdan gelenler
+                string path = @"\\35TKNTFS1\\YNA_SQL_Script\\";
+                string path2 = DateTime.Now.ToString("dd.MM.yyyy");
+                string FileName = path + DateTime.Now.ToString("dd.MM.yyyy");
+                string NewFile = textBox1.Text;
+                string temp = NewFile.Replace("YNAFARK.", "");
+                SaveFileDialog a1 = new SaveFileDialog();
+                a1.Filter = "Yazı Dosyaları(*txt)|*.txt";
+                a1.DefaultExt = "txt";
+                
+                for (int i = 1; ; i++)
+                {
+                    DirectoryInfo hdDirectoryInWhichToSearch = new DirectoryInfo(path);
+                    FileInfo[] filesInDir = hdDirectoryInWhichToSearch.GetFiles(path2 + "-" + i.ToString() + "*");
+
+                    if (filesInDir.Count() == 0)
+                    {
+                        FileStream fs1 = new FileStream(FileName + "-" + i + ".txt", FileMode.OpenOrCreate, FileAccess.Write);
+                        StreamWriter yazmaislemi = new StreamWriter(fs1);
+                        yazmaislemi.WriteLine(temp);
+                        yazmaislemi.Close();
+                        if(textBox1.Text == "")
+                      {
+                          File.Delete(FileName + "-" + i + ".txt");
+                          MessageBox.Show("Yazı girilmedi");
+                      }
+                        else   {  
+                        MessageBox.Show("Kaydedilmiştir");
+                        textBox1.Clear();
+                        }
+                        return;
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.ToString());
+                throw ex;
+
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //ENSON ve ENBAS
+
+                string radioButtonChoose = ("");
+                if (radioButton1.Checked == true)
+
+                    radioButtonChoose = radioButton1.Text;
+                else
+                    radioButtonChoose = radioButton2.Text;
+
+               
+                string ByWho = textBox3.Text;
+                string Final1 = "-----------" + ByWho + "\r\n";
+                string delete = textBox2.Text;
+                string Final2 = Final1 + delete.Replace("YNATEST.", ""); ;
+
+                string path = @"\\35TKNTFS1\\YNA_SQL_Script\\";
+                string path2 = DateTime.Now.ToString("dd.MM.yyyy");
+                string FileName = path + DateTime.Now.ToString("dd.MM.yyyy");
+                SaveFileDialog a2 = new SaveFileDialog();
+
+                a2.FileName = "";
+                a2.Filter = "Yazı Dosyaları(*txt)|*.txt";
+                a2.DefaultExt = "txt";
+
+                if(radioButton1.Checked ==false && radioButton2.Checked ==false )
+                {
+
+                MessageBox.Show("Seçim yapmadınız");
+                
+                }
+                for (int i = 1; ; i++)
+                {
+                    DirectoryInfo hdDirectoryInWhichToSearch = new DirectoryInfo(path);
+                    FileInfo[] filesInDir = hdDirectoryInWhichToSearch.GetFiles(path2 + "-" + i.ToString() + "*");
+
+                    if (filesInDir.Count() == 0)
+                    {
+                        FileStream fs1 = new FileStream(FileName + "-" + i + radioButtonChoose + "_" + ByWho + ".txt", FileMode.OpenOrCreate, FileAccess.Write);
+                        StreamWriter yazmaislemi = new StreamWriter(fs1);
+                        yazmaislemi.WriteLine(Final2);
+                        yazmaislemi.Close();
+                        if (radioButton1.Checked == false && radioButton2.Checked == false)
+                        {
+                            File.Delete(FileName + "-" + i + radioButtonChoose + "_" + ByWho + ".txt");
+                            MessageBox.Show("Seçim yapmadınız");
+
+                        }
+                        else if (textBox2.Text == "" || textBox3.Text == "")
+                        {
+                            File.Delete(FileName + "-" + i + radioButtonChoose + "_" + ByWho + ".txt");
+                            MessageBox.Show("Yazı Girilmedi");
+
+                        }
+                        else
+                        {
+                            MessageBox.Show("Kaydedilmiştir");
+                            textBox2.Clear();
+                            textBox3.Clear();
+                        }
+                        return;
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                throw ex;
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Haftalık ve History klasörüne gönderme
+
+                //ENBAS
+
+                string newpath = @"\\35TKNTFS1\\YNA_SQL_Script\\HaftaBazli\\";
+                string path = @"\\35TKNTFS1\\YNA_SQL_Script\\";
+                DirectoryInfo merge = new DirectoryInfo(path);
+                FileInfo[] filename1 = merge.GetFiles("*ENBAS*").OrderBy(p => p.CreationTime).ToArray();
+
+                SaveFileDialog a1 = new SaveFileDialog();
+                a1.Filter = "Yazı Dosyaları(*txt)|*.txt";
+                a1.DefaultExt = "txt";
+                FileStream fs1 = new FileStream(newpath + DateTime.Now.Year+ "-" + GetWeekNumber(DateTime.Now) + "ENBAS" + ".txt", FileMode.Create, FileAccess.Write);
+                StreamWriter yazmaislemi = new StreamWriter(fs1);
+                
+                if (filename1.Count() == 0)
+                {
+                    yazmaislemi.Close();
+                    File.Delete(newpath + DateTime.Now.Year + "-" + GetWeekNumber(DateTime.Now) + "ENBAS" + ".txt");
+                }
+
+
+
+                else
+                {
+                    for (int i = 0; i < filename1.Count(); i++)
+                    {
+                        string linesFinal = "";
+                        string[] lines = System.IO.File.ReadAllLines(filename1[i].FullName);
+                        for (int j = 0; j < lines.Count(); j++)
+                        {
+                            linesFinal = linesFinal + lines[j].ToString() + System.Environment.NewLine;
+
+                        }
+                        yazmaislemi.WriteLine(linesFinal);
+
+
+                    }
+                    yazmaislemi.Close();
+                }
+                string from = @"\\35TKNTFS1\\YNA_SQL_Script\\";
+                string destination = @"\\35TKNTFS1\\YNA_SQL_Script\\History\\";
+                foreach (var file in Directory.GetFiles(from, "*ENBAS*"))
+                {
+                    string FileName = Path.GetFileName(file);
+                    File.Move(file, Path.Combine(destination, FileName));
+                }
+
+                //ENSON
+
+                SaveFileDialog a2 = new SaveFileDialog();
+                a2.Filter = "Yazı Dosyaları(*txt)|*.txt";
+                a2.DefaultExt = "txt";
+                string newpath2 = @"\\35TKNTFS1\\YNA_SQL_Script\\HaftaBazli\\";
+                string path2 = @"\\35TKNTFS1\\YNA_SQL_Script\\";
+                DirectoryInfo merge2 = new DirectoryInfo(path2);
+                
+                FileInfo[] filename2 = merge2.GetFiles("*ENSON*").OrderBy(p => p.CreationTime).ToArray();
+                FileStream fs2 = new FileStream(newpath2 + DateTime.Now.Year + "-" + GetWeekNumber(DateTime.Now) + "ENSON" + ".txt", FileMode.Create, FileAccess.Write);
+                StreamWriter yazmaislemi2 = new StreamWriter(fs2);
+
+                if (filename2.Count() == 0)
+                {
+                    yazmaislemi2.Close();
+                    File.Delete(newpath + DateTime.Now.Year + "-" + GetWeekNumber(DateTime.Now) + "ENSON" + ".txt");
+                }
+
+                else
+                {
+                    for (int i = 0; i < filename2.Count(); i++)
+                    {
+                        string linesFinal = "";
+                        string[] lines = System.IO.File.ReadAllLines(filename2[i].FullName);
+                        for (int j = 0; j < lines.Count(); j++)
+                        {
+                            linesFinal = linesFinal + lines[j].ToString() + System.Environment.NewLine;
+
+                        }
+                        yazmaislemi2.WriteLine(linesFinal);
+
+
+                    }
+                    yazmaislemi2.Close();
+                }
+                string from2 = @"\\35TKNTFS1\\YNA_SQL_Script\\";
+                string destination2 = @"\\35TKNTFS1\\YNA_SQL_Script\\History\\";
+                foreach (var file in Directory.GetFiles(from2, "*ENSON*"))
+                {
+                    string FileName = Path.GetFileName(file);
+                    File.Move(file, Path.Combine(destination2, FileName));
+                }
+
+                //TOAD
+
+                SaveFileDialog a3 = new SaveFileDialog();
+                a3.Filter = "Yazı Dosyaları(*txt)|*.txt";
+                a3.DefaultExt = "txt";
+
+
+                string newpath3 = @"\\35TKNTFS1\\YNA_SQL_Script\\HaftaBazli\\";
+                string path3 = @"\\35TKNTFS1\\YNA_SQL_Script\\";
+                DirectoryInfo merge3 = new DirectoryInfo(path3);
+
+                FileStream fs3 = new FileStream(newpath3 + DateTime.Now.Year + "-" + GetWeekNumber(DateTime.Now) + ".txt", FileMode.Create, FileAccess.Write);
+                StreamWriter yazmaislemi3 = new StreamWriter(fs3);
+
+
+                FileInfo[] filename3 = merge3.GetFiles("*.txt").OrderBy(p => p.CreationTime).ToArray();
+                if (filename3.Count() == 0)
+                {
+                    yazmaislemi3.Close();
+                    File.Delete(newpath + DateTime.Now.Year + "-" + GetWeekNumber(DateTime.Now) + ".txt");
+                }
+
+                else
+                {
+
+                    for (int i = 0; i < filename3.Count(); i++)
+                    {
+                        string linesFinal = "";
+                        string[] lines = System.IO.File.ReadAllLines(filename3[i].FullName);
+                        for (int j = 0; j < lines.Count(); j++)
+                        {
+                            linesFinal = linesFinal + lines[j].ToString() + System.Environment.NewLine;
+
+                        }
+                        yazmaislemi3.WriteLine(linesFinal);
+
+
+                    }
+                    yazmaislemi3.Close();
+                }
+                string from3 = @"\\35TKNTFS1\\YNA_SQL_Script\\";
+                string destination3 = @"\\35TKNTFS1\\YNA_SQL_Script\\History\\";
+                foreach (var file in Directory.GetFiles(from3, "*.txt"))
+                {
+                    string FileName = Path.GetFileName(file);
+                    File.Move(file, Path.Combine(destination3, FileName));
+
+                }
+                MessageBox.Show("Kaydedilmiştir");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                throw ex;
+
+            }
+
+
+        }
+
+        //tarihin yılın kaçıncı haftasında olduğunu bulan method
+        public static int GetWeekNumber(DateTime dtPassed)
+        {
+            CultureInfo ciCurr = CultureInfo.CurrentCulture;
+            int weekNum = ciCurr.Calendar.GetWeekOfYear(dtPassed, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
+            return weekNum;
+        }
+
+    }
+}
+
